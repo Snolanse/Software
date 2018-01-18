b62ce67e1b13e80095ec16d6446987731c77dee6
Tjore <35220757+Tjore@users.noreply.github.com>
2018-01-18 08:13:22 +0100

    PI-V0.1
    Skrevet funksjon som rekner ut wetbulb
    Startet på PI-regulatoren

--- Python_T/værdata.py
+++ Python_T/værdata.py
@@ -7,9 +7,10 @@ Created on Tue Jan 16 16:52:48 2018
 
 import requests
 requests.packages.urllib3.disable_warnings()
+import numpy
 
 def fåVærData():
-    
+    """Henter data fra værstasjon via server"""
     URL = "https://feed.metnet.no/current/"
     
     header = {'token': "FeLQrwndUyuE5mRuuPNJ5BifQtCrcO",
@@ -21,3 +22,14 @@ def fåVærData():
     
     return minRequest.json()['data']
 
+def wetbulb(temp,hum):
+    """rekner ut wet-bulb tempratur ut fra lufttempratur og luftfuktighet""" 
+    wbulb = (20*numpy.arctan(0.151977*(hum + 8.313659)**0.5)
+            + numpy.arctan(temp + hum)
+            - numpy.arctan(hum - 1.676331)
+            + 0.00391838*hum**(3/2)*numpy.arctan(0.023101*hum)
+            - 4.686035)
+    
+    wbulb = round(float(wbulb),1)
+    
+    return wbulb
