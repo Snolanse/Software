9f6fe3c4c70cd07cd9054881c22189360997b77f
Tjore <35220757+Tjore@users.noreply.github.com>
2018-03-05 10:20:39 +0100

    Django Start
    Enkel django "server" til testing

---
+++ Python_T/GG_good_try/mysite/accounts/static/accounts/JStest.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+
+function pertest() {
+    document.getElementById('test').innerHTML = "Per er en ganske kul type"
+}
---
+++ Python_T/GG_good_try/mysite/startside/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StartsideConfig(AppConfig):
+    name = 'startside'
