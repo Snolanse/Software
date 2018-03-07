6d9a22b7e2084d9e7dc16ffd98e5127865ce8995
MikJaw <mikolajj@stud.ntnu.no>
2018-03-07 16:11:46 +0100

    Endring kommentering

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,14 +1,14 @@
-#Imports----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Imports----------------------------------------------------------------------------------------------------------------
 import tkinter as tk
 import os
 
-#Global variables-------------------------------------------------------------------------------------------------------------------------------------------------------
+#Global variables-------------------------------------------------------------------------------------------------------
 lanceType = None
 creds = 'tempfile.temp' # Variable that becomes login data document
 LARGE_FONT = ("Verdana", 12)# Used to store font type
 
 
-#Functions -------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Functions -------------------------------------------------------------------------------------------------------------
 
 def CheckLogin(cntrl):# Used to check if username and password is correct.
     with open(creds) as f:
@@ -16,7 +16,8 @@ def CheckLogin(cntrl):# Used to check if username and password is correct.
         uname = data[0].rstrip()  # Data[0], 0 is the first line, 1 is the second and so on.
         pword = data[1].rstrip()  # Using .rstrip() will remove the \n (new line) word from before when we input it
 
-    if ((nameEL.get() == "admin" and pwordEL.get() == "admin") or (nameEL.get() == uname and pwordEL.get() == pword)):  # Checks to see if you entered the correct data.
+    if ((nameEL.get() == "admin" and pwordEL.get() == "admin") or
+            (nameEL.get() == uname and pwordEL.get() == pword)):  # Checks to see if you entered the correct data.
         cntrl.show_frame(SnTypePage)
     else:
         cntrl.show_frame(LoginF)
@@ -29,14 +30,15 @@ def lancetype(Type, cntrl):#Used to change value of global variable, and change 
 def FSSignup(cntrl):# Used to add a user to the GUI. So far only one user can be added.
     with open(creds, 'w') as f:  # Creates a document using the variable we made at the top.
         f.write(
-            nameE.get())  # nameE is the variable we were storing the input to. Tkinter makes us use .get() to get the actual string.
+            nameE.get())            '''nameE is the variable we were storing the input to.
+                                    Tkinter makes us use .get() to get the actual string. '''
         f.write('\n')  # Splits the line so both variables are on different lines.
         f.write(pwordE.get())  # Same as nameE just with pword var
         f.close()  # Closes the file
     cntrl.show_frame(Login)
 
 
-#Methods----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Methods----------------------------------------------------------------------------------------------------------------
 class AppGui(tk.Tk):# Main GUI class
 
     def __init__(self, *args, **kwargs):
@@ -51,7 +53,7 @@ class AppGui(tk.Tk):# Main GUI class
 
         self.frames = {}
 
-        for F in (Signup, Login, SnTypePage, LoginF, PlacementPage, MainPage):# for loop to include all pages that are made
+        for F in (Signup, Login, SnTypePage, LoginF, PlacementPage, MainPage):# Includes all pages
 
             frame = F(container, self)
 
@@ -191,7 +193,7 @@ class MainPage(tk.Frame):# Main page
         button2.pack()
 
 
-#"Main loop"-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#"Main loop"------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     app = AppGui()
     app.mainloop()
