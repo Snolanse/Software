60561db5b558d6cce696f093e8b36d816ed9de71
Tjore <35220757+Tjore@users.noreply.github.com>
2018-03-20 15:34:20 +0100

    Sjango
    DJAAANNGOOOO

--- Python_T/lanseside/startside/views.py
+++ Python_T/lanseside/startside/views.py
@@ -9,83 +9,83 @@ try:
     from modules.Blinky import on_off
 except:
     print('Fikk ikke lasta blinky')
-finally:
 
-    # Create your views here.
 
-    @login_required
-    @ensure_csrf_cookie
-    def info(request):
-        if request.method == 'GET':
-            lanse = Lanse.objects.get(lanse_nr = 1)
-            lanse = vars(lanse)
-            del lanse['_state']
-            return JsonResponse(lanse)
-        if request.method == 'POST':
-            lanse = Lanse.objects.get(lanse_nr = 1)
-            lanse = vars(lanse)
-            del lanse['_state']
-            return JsonResponse(lanse)
+# Create your views here.
 
-    @ensure_csrf_cookie
-    def startside(request):
-        return(render(request, 'startside/startside.html'))
+@login_required
+@ensure_csrf_cookie
+def info(request):
+    if request.method == 'GET':
+        lanse = Lanse.objects.get(lanse_nr = 1)
+        lanse = vars(lanse)
+        del lanse['_state']
+        return JsonResponse(lanse)
+    if request.method == 'POST':
+        lanse = Lanse.objects.get(lanse_nr = 1)
+        lanse = vars(lanse)
+        del lanse['_state']
+        return JsonResponse(lanse)
 
-    @ensure_csrf_cookie
-    def test(request):
-        if request.method == 'GET':
-            led = LED.objects.all()[0]
-            state = led.stat
-            args = {
-                'state': state}
-            return(render(request, 'test/test.html',args))
-        elif request.method == 'POST':
-            if hasattr(request.POST,'id'):
-                id = request.POST['id']
-                led = LED.objects.get(id=1) 
-                if hasattr(request.POST,'state'):
-                    led.stat = request.POST['state']
-                    led.save()
-                    try:
-                        on_off(led.stat)
-                    except:
-                        print('fikk ikke slått på led')
-                del led['_state']
-                return JsonResponse()
-        else:
-            return HttpResponse('')
+@ensure_csrf_cookie
+def startside(request):
+    return(render(request, 'startside/startside.html'))
 
-    @ensure_csrf_cookie
-    def lanser(request):
-        if request.method == 'GET':
-            return(render(request, 'startside/lanser.html'))
-        elif request.method == 'POST':
-            x = int(request.POST['lanse'])
-            lanse = Lanse.objects.get(lanse_nr = x)
-            lanse = vars(lanse)
-            del lanse['_state']
-            return JsonResponse(lanse)
+#@ensure_csrf_cookie
+def test(request):
+    if request.method == 'GET':
+        led = LED.objects.all()[0]
+        state = led.stat
+        args = {
+            'state': state}
+        return(render(request, 'test/test.html',args))
+    elif request.method == 'POST':
+        if hasattr(request.POST,'id'):
+            id = request.POST['id']
+            led = LED.objects.get(id=1) 
+            if hasattr(request.POST,'state'):
+                led.stat = request.POST['state']
+                led.save()
+                try:
+                    on_off(led.stat)
+                except:
+                    print('fikk ikke slått på led')
+            del led['_state']
+            return JsonResponse()
+    else:
+        return HttpResponse('')
 
-    @ensure_csrf_cookie
-    def valgtlanse(request):
-        if request.method == 'GET':
-            return JsonResponse({'info': 'dette var en get'})
-        if request.method == 'POST':
-            try:
-                data = request.POST['test']
-                print(data)
-                lanse_nr = int(data[(data.find('Lanse'))+5:])
-                print(lanse_nr)
-                lanse = Lanse.objects.all().order_by('lanse_nr')[lanse_nr-1]
-                ant_steg = lanse.ant_steg
-            except:
-                data = 'ingen lanse'
-                ant_steg = 0
-            finally:
-                args = {'data': data,
-                        'ant_steg': ant_steg
-                        }
-                #return JsonResponse(args)
-                return(render(request, 'lansestyring/lansestyring.html', args))
-        else:
-            return HttpResponse('')
+@ensure_csrf_cookie
+def lanser(request):
+    if request.method == 'GET':
+        return(render(request, 'startside/lanser.html'))
+    elif request.method == 'POST':
+        x = int(request.POST['lanse'])
+        lanse = Lanse.objects.get(lanse_nr = x)
+        lanse = vars(lanse)
+        del lanse['_state']
+        return JsonResponse(lanse)
+
+@ensure_csrf_cookie
+def valgtlanse(request):
+    if request.method == 'GET':
+        return JsonResponse({'info': 'dette var en get'})
+    if request.method == 'POST':
+        try:
+            data = request.POST['test']
+            print(data)
+            lanse_nr = int(data[(data.find('Lanse'))+5:])
+            print(lanse_nr)
+            lanse = Lanse.objects.all().order_by('lanse_nr')[lanse_nr-1]
+            ant_steg = lanse.ant_steg
+        except:
+            data = 'ingen lanse'
+            ant_steg = 0
+        finally:
+            args = {'data': data,
+                    'ant_steg': ant_steg
+                    }
+            #return JsonResponse(args)
+            return(render(request, 'lansestyring/lansestyring.html', args))
+    else:
+        return HttpResponse('')
