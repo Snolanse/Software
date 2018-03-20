76a8a341bdb869904ac8eb39384c7c7e4d884ccf
Tjore <35220757+Tjore@users.noreply.github.com>
2018-03-20 14:26:39 +0100

    Django
    Rolig start på å låse tilgangen på nettsider slik at man må logge inn
    
    Koblet opp en led på raspberry som skal kontrolleres fra nettsiden

--- Python_T/lanseside/startside/static/base.js
+++ Python_T/lanseside/startside/static/base.js
@@ -7,3 +7,19 @@ setTimeout(pressknapp, 20);
 function pressknapp() {
     document.getElementById(document.getElementById("tittel").innerHTML).setAttribute("class", 'active');
 }
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Starter stringen til cookien med navnet vi vil ha?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
--- Python_T/lanseside/startside/urls.py
+++ Python_T/lanseside/startside/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r"^lanser$", views.lanser, name='lanser'),
     url(r"^valgtlanse$", views.valgtlanse, name='valgtlanse'),
     url(r"^info$", views.info, name='infotest'),
+    url(r"^test$", views.test, name='test'),
   ]
