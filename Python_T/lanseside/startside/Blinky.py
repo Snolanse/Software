55352fce580962369003e0f4f7ea2d74001b2726
Tjore <35220757+Tjore@users.noreply.github.com>
2018-03-20 15:00:18 +0100

    Blinky

---
+++ Python_T/lanseside/startside/Blinky.py
@@ -0,0 +1,15 @@
+
+def __init__():
+    import reprlib.GPIO as GPIO
+    LedPin = 11
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(LedPin, GPIO.OUT)
+    GPIO.output(LedPin, GPIO.HIGH)
+
+def on_off(i):
+    if i == 1:
+        GPIO.output(LedPin, GPIO.HIGH)
+        print('led on')
+    else:
+        GPIO.output(LedPin, GPIO.LOW)
+        print('led off')
